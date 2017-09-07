1d3a2bc4d0839e7a0fe467f754ec0b318901e1c7
ARMouser <armouser@gmail.com>
2017-09-07 12:25:11 -0500

    Delete keys.js

--- keys.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: 'XVrFkVmMrVXfXvbO13obiDwle',
-  consumer_secret: 'a67O1bzxEZ0dHd5B6yOkAd3dt6Z1hdCFv4bYrj7kcIEbBjFgzT',
-  access_token_key: '58535655-TERHJCpzulzcE0IDmvEpAsaqXC2cb7ctFWH8863LA',
-  access_token_secret: 'Mw3H9lX4q7PfvwGaAIknqn20VFsjojRW1X6pZB8hzXTAq',
-}
